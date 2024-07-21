3dce8bfa4098199243496a8afb18ef817f073222
asmaismail <asmaismail202020@gmail.com>
2024-07-21 22:09:10 +0500

    upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.banner-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    fade: true
+  });
+});
