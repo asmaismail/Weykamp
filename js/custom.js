27d8079edeb03887bf10a54f3cb412fcb748fc31
asmaismail <asmaismail202020@gmail.com>
2024-07-22 15:38:08 +0500

    upload

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,27 @@ $(document).ready(function() {
     fade: true
   });
 });
+// footer
+document.addEventListener('DOMContentLoaded', function () {
+  const background = document.querySelector('.background');
+  const dotSize = 2; 
+  const spacing = 12; 
+  background.style.backgroundImage = `radial-gradient(#555 ${dotSize}px, transparent ${dotSize}px)`;
+  background.style.backgroundSize = `${spacing}px ${spacing}px`;
+});
+//img filter
+$(document).ready(function() {
+  $('.img-container').addClass('show');
+  $('.filter-btn').click(function() {
+      var filterValue = $(this).attr('data-filter');
+      $('.filter-btn').removeClass('filter-active');
+      $(this).addClass('filter-active');
+      if (filterValue == 'all') {
+          $('.img-container').addClass('show');
+      } else {
+          $('.img-container').removeClass('show');
+          $('.' + filterValue).addClass('show');
+      }
+  });
+});
+//
